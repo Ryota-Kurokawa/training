ee18e42425f17dc7d9640e2b2b5c048ad3440b89
ryota <kurokawa.ryota.1124@gmail.com>
2024-02-25 18:10:20 +0900

    initialization

---
+++ training/TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainingApp.swift
+//  training
+//
+//  Created by 黒川良太 on 2024/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
